d6e7721cce20c396d47858cd41c637b9572e4df6
Łukasz Witek <lukasz.witek123@gmail.com>
2017-12-01 17:23:14 +0100

    logService

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,25 +1,14 @@
 package com.example.demo;
 
-import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-
-@Controller
+@RestController
 public class HomeController {
 
     @RequestMapping("/")
-        public String hello(Model model){
-
-            Credentials credentials = new Credentials();
-
-            model.addAttribute("cred",credentials);
-
-            return "homepage";
-        }
-
+    public String home(){
+        return "Hello World";
+    }
 }
